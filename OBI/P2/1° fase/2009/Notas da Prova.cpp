fe33f6f1928891633a18d46bcb06f38fbb0e36fd
Pijuma <73311085+pijuma@users.noreply.github.com>
2021-06-11 09:51:12 -0300

    Create Notas da Prova.cpp

---
+++ OBI/P2/1° fase/2009/Notas da Prova.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a;
+int main(){
+    cin>>a;
+    if(a>=86) cout<<"A";
+    else if(a>=61) cout<<"B";
+    else if(a>=36) cout<<"C";
+    else if(a>=1) cout<<"D";
+    else cout<<"E";
+}
