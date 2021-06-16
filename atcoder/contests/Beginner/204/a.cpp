2d1156907ff5c4d0f3f61f49773f26ecd2fd1a67
Pijuma <73311085+pijuma@users.noreply.github.com>
2021-06-16 16:06:23 -0300

    Create a.cpp

---
+++ atcoder/contests/Beginner/204/a.cpp
@@ -0,0 +1,13 @@
+    #include<bits/stdc++.h>
+    using namespace std ; 
+     
+    int v[3] ;
+     
+    int main(){
+     
+    	cin >> v[1] >> v[2] ; 
+     
+    	if(v[1] == v[2]) cout << v[1] << "\n" ; 
+    	else cout << (3 - v[1] - v[2]) << "\n" ; 
+    	
+    }
