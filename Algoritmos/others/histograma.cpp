dbb478fad3bfbe58e0fad777b0e359e4b5873176
Pijuma <73311085+pijuma@users.noreply.github.com>
2021-06-28 14:23:57 -0300

    Create histograma.cpp

---
+++ Algoritmos/others/histograma.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+const int maxn = 1e5 + 5 ; 
+
+int n, h[maxn], esq[maxn], dir[maxn] ; 
+
+int main(){
+
+	cin >> n ; 
+
+	for(int i = 1 ; i <= n ; i++) cin >> h[i] ; 
+
+	esq[0] = dir[n+1] = -1 ; 
+    
+    for(int i = 1 ; i <= n ; i++){
+    	esq[i] = i-1 ; 
+    	while(h[esq[i]] >= h[i]) esq[i] = esq[esq[i]] ; 
+    }
+
+    for(int i = n ; i > 0 ; i--){
+    	dir[i] = i+1 ; 
+    	while(h[dir[i]] >= h[i]) dir[i] = dir[dir[i]] ; 
+    }
+
+    int mx = 0 ; 
+    
+    for(int i = 1 ; i <= n ; i++){
+    	mx = max(mx, (dir[i] - esq[i] - 1)*h[i]) ; 
+    }
+
+    cout << mx << "\n" ; 
+
+}
