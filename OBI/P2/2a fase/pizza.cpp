312ba2c6f0575e0e5573e98ef2b92194e1e1e8a3
Pijuma <73311085+pijuma@users.noreply.github.com>
2021-08-07 13:39:47 -0300

    Create pizza.cpp

---
+++ OBI/P2/2a fase/pizza.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+const int maxn = 2e5 + 5 ; 
+
+int n, maior, resp, resp2, maior2, v[maxn], soma, vec[maxn] ; 
+
+int main(){
+
+	cin >> n ; 
+
+	for(int i = 1 ; i <= n ; i++){
+		cin >> v[i] ; 
+		vec[i] = -v[i] ;  
+		soma += vec[i] ; 
+	}
+
+	for(int i = 1 ; i <= n ; i++){
+		maior = max(maior + vec[i], 0) ; 
+		resp = max(resp, maior) ; 
+		maior2 = max(0, maior2 + v[i]) ; 
+		resp2 = max(resp2, maior2) ; 
+	}
+
+	cout << max(resp2, resp - soma) << "\n" ; 
+
+}
