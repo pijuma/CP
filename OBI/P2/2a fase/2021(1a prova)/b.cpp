1531d7e275553282c261934d24db8c32c0592af5
Pijuma <73311085+pijuma@users.noreply.github.com>
2021-08-21 14:24:03 -0300

    Create b.cpp

---
+++ OBI/P2/2a fase/2021(1a prova)/b.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define int long long 
+using namespace std ; 
+
+const int inf = 1e9 + 2 ; 
+
+// é dado um a e um b e queremos dizer o menor c 
+// tal que a media e mediana sejam iguais 
+// (a + b + c)/3 = carinha do meio 
+
+int a, b, c ; 
+
+int32_t main(){
+
+	ios_base::sync_with_stdio(false) ; cin.tie(NULL) ; cout.tie(NULL) ; 
+
+	cin >> a >> b ; 
+
+	cout << (2*a)-b << "\n" ; 
+}
