ba54c936eb9bad315d385d3149c05ba926448f4a
Pietra <73311085+pijuma@users.noreply.github.com>
2022-01-25 15:23:32 -0300

    Create K.cpp

---
+++ summerschool2022/Brazilian class/Augusto/Contest 2/K.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define int long long 
+using namespace std ; 
+
+int t, n, x, y ; 
+
+int32_t main(){
+
+	cin >> t ; 
+
+	while(t--){
+		cin >> n >> x >> y ;
+		cout << max({n, x, y}) << "\n" ; 
+	}
+
+
+}
