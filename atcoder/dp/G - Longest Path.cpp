ba77c94b27211f82f138f15b3b7a19b01ef8b36b
Pietra <73311085+pijuma@users.noreply.github.com>
2022-03-05 22:41:32 -0300

    Update G - Longest Path.cpp

--- atcoder/dp/G - Longest Path.cpp
+++ atcoder/dp/G - Longest Path.cpp
@@ -3,17 +3,19 @@ using namespace std ;
 
 const int maxn = 1e5 + 5 ; 
 
-int n, m, vis[maxn], a, b, dp[maxn] ; 
+int n, m, dp[maxn], ans, vis[maxn] ; 
 vector<int> grafo[maxn] ; 
 
-void dfs(int v){
+void dfs(int v, int p){
 
 	vis[v] = 1 ; 
 
-	for(int a : grafo[v]){
-		if(!vis[a]) dfs(a) ;  
-		dp[v] = max(dp[v], dp[a] + 1) ;  
+	for(auto a : grafo[v]){
+		if(!vis[a]) dfs(a, v) ; 
+		dp[v] = max(dp[v], dp[a] + 1) ; 
 	}
+
+	ans = max(ans, dp[v]) ; 
 
 }
 
@@ -21,16 +23,12 @@ int main(){
 
 	cin >> n >> m ; 
 
-	for(int i = 1 ; i  <= m ; i++){
-		cin >> a >> b ; 
+	for(int i = 1 ; i <= m ; i++){
+		int a, b ; cin >> a >> b ; 
 		grafo[a].push_back(b) ; 
 	}
 
-	for(int i = 1 ; i <= n ; i++) if(!vis[i]) dfs(i) ; 
-
-	int ans = 0 ; 
-
-	for(int i = 1 ; i <= n ; i++) if(dp[i] > ans) ans = dp[i] ; 
+	for(int i = 1 ; i <= n ; i++) if(!vis[i]) dfs(i, i) ; 
 
 	cout << ans << "\n" ; 
 
